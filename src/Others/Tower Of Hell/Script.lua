5b114af1f55546554dcb907ffba715fb77c37665
AltLexon <93941688+AltLexon@users.noreply.github.com>
2023-02-17 15:41:32 -0300

    Create Github

---
+++ src/Others/Tower Of Hell/Script.lua
@@ -0,0 +1,7 @@
+return function()
+  --require(script.Parent["Anti-Cheat"])()
+  print('Version: '..require(script.Parent.version))
+
+
+  print('Script Executed!')
+end
---
+++ src/init.client.lua
@@ -0,0 +1,7 @@
+local _ScriptHubName = 'Fluid Hub'
+
+local GamesSupport = {
+  'Tower Of Hell'
+}
+
+require(script.Others["Tower Of Hell"].Script)()
